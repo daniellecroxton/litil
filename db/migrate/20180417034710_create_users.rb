a5344d3eaf78b03c43b91812d045ed0e60107839
daniellecroxton <danikrist@gmail.com>
2018-04-16 22:12:38 -0600

    Installed rspec-rails gem

--- db/migrate/20180417034710_create_users.rb
+++ db/migrate/20180417034710_create_users.rb
@@ -4,7 +4,6 @@ class CreateUsers < ActiveRecord::Migration[5.1]
       t.string :name
       t.string :password_digest
       t.string :email
-      t.integer :business_id
 
       t.timestamps
     end
