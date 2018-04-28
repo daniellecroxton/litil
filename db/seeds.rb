e12872289d09b031aa76da3b469191b42eaaa661
daniellecroxton <danikrist@gmail.com>
2018-04-28 14:57:42 -0600

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  User.create(
+    name: Faker::Name.name,
+    email: Faker::Internet.unique.email,
+    password: "password",
+    password_confirmation: "password"
+  )
+
+10.times do
+  Business.create(
+    name: Faker::Company.name,
+    full_street_address: Faker::Address.street_address.city.state_abbr.zip,
+    website: Faker::Internet.url,
+    phone: Faker::PhoneNumber.phone_number
+  )
+end
+
+# 10.times do
+#   Item.create(
+#     title: Faker::Commerce.product_name,
+#     inventory: Faker::Number.number(2),
+#     price: Faker::Number.decimal(2)
+#   )
+#   Category.create(title: Faker::Commerce.department)
+# end
+#
+# counter = 1
+# Item.all.each do |item|
+#   item.category_id = counter
+#   item.save
+#   counter += 1
+# end
+#
+# 5.times do
+#   User.create(
+#     name: Faker::Name.name,
+#     email: Faker::Internet.email,
+#     password: "password",
+#     password_confirmation: "password"
+#   )
+# end
