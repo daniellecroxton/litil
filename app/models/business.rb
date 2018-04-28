16050393c7c2d3af917358ecf4fe095c9f929c5d
daniellecroxton <danikrist@gmail.com>
2018-04-27 22:15:23 -0600

    Added join table associations to models

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,6 +1,7 @@
 class Business < ApplicationRecord
   belongs_to :user
   belongs_to :category
-  # has_many :products, through: :bu
+  has_many :businesses_products
+  has_many :products, through: :businesses_products
   mount_uploader :image, ImageUploader
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ApplicationRecord
-  has_many :product_tags
-  has_many :tags, through: :product_tags
+  has_many :products_tags
+  has_many :tags, through: :products_tags
+  has_many :businesses_products
+  has_many :businesses, through: :businesses_products
   mount_uploader :image, ImageUploader
 
 end
