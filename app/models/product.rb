b0392af2f42164a92122f110be5f15a50490cb08
daniellecroxton <danikrist@gmail.com>
2018-04-28 14:36:47 -0600

    Adjusted associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,5 @@
 class Product < ApplicationRecord
-  has_many :products_tags
-  has_many :tags, through: :products_tags
+  has_and_belongs_to_many :tags
   has_many :businesses_products
   has_many :businesses, through: :businesses_products
   mount_uploader :image, ImageUploader
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,3 @@
 class Tag < ApplicationRecord
-  has_many :products_tags
-  has_many :products, through: :products_tags
+  has_and_belongs_to_many :products
 end
