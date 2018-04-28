87fe08ac7ef1a2e0ae8d286f2bb1568ba0d5e507
daniellecroxton <danikrist@gmail.com>
2018-04-28 15:14:11 -0600

    Created index page to view seed data

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,2 +1,9 @@
 class BusinessesController < ApplicationController
+  def index
+    @businesses = Business.all
+    @products = Product.all
+    @tags = Tag.all
+    @categories = Category.all
+    @user = User.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root to: "businesses#index" 
+  root to: "application#index" 
   resources :businesses
   resources :tags
   resources :products
