8f77afffc42b376697d45e4e5b70db511598eeba
daniellecroxton <danikrist@gmail.com>
2018-04-29 14:39:08 -0600

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root to: "application#index" 
+  root to: "application#index"
   resources :businesses
   resources :tags
   resources :products
   resources :categories
-  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
