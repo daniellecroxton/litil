439a4f457bcfc828b5fa15d22351a69e85235e03
daniellecroxton <danikrist@gmail.com>
2018-05-01 16:25:15 -0600

    Created form for new business

--- app/models/business.rb
+++ app/models/business.rb
@@ -5,7 +5,14 @@ class Business < ApplicationRecord
   has_many :products, through: :businesses_products
   mount_uploader :image, ImageUploader
   scope :recent, -> { order(:created_at, :desc) }
-
+  # accepts_nested_attributes_for :category
+  #
+  # def category_attributes=(category_attributes)
+  #   category_attributes.values.each do |category_attribute|
+  #     category = Category.find_or_create_by(category_attribute)
+  #     self.category_id = category
+  #   end
+  # end
 
   def self.by_category(category_id)
     self.where(:category_id => category_id)
