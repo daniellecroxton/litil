ae1c98d59bb839eae1b0992db09551a060799cbb
daniellecroxton <danikrist@gmail.com>
2018-05-03 21:33:14 -0600

    added custom user show page

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   end
   resources :tags
   resources :categories, only: [:index, :show]
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
