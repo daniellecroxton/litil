b195b5673c1cd1f989a157d7738aca42706240c2
daniellecroxton <danikrist@gmail.com>
2018-05-05 21:46:17 -0600

    Added test to Category spec

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -6,6 +6,14 @@ RSpec.describe Category, :type => :model do
     @category.businesses << Business.first
   end
 
+  it "is valid with a name" do
+    expect(@category).to be_valid
+  end
+
+  it "is not valid without a name" do
+    expect(Category.new(name: nil)).not_to be_valid
+  end
+
   it 'has many businesses' do
     expect(@category.businesses).to include(Bustiness.first)
   end
