481a7c2dc2da18696e7a718b546726b28fd07997
daniellecroxton <danikrist@gmail.com>
2018-05-19 16:02:35 -0600

    updated tag create method

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,20 @@
 class TagsController < ApplicationController
 
   def new
+    @tag = Tag.new
+  end
+
+  def create
+    @tag = Tag.new
+    #make current_product helper method
+    respond_to do |format|
+      if @tag.save
+        current_product << @tag
+        format.html { redirect_to product_path(current_product), notice: "Tag added." }
+      else
+        format.html { render :new }
+      end
+    end
   end
 
   def update
