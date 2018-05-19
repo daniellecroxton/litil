6be3293bd94cec29b1cb084c2829dba129d5b1e0
daniellecroxton <danikrist@gmail.com>
2018-05-19 16:30:06 -0600

    Updated product create action and form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,6 +12,24 @@ class ProductsController < ApplicationController
     @business = Business.find_by_id(params[:business][:id])
   end
 
+  def create
+    # raise current_user.inspect
+    @product = Product.new(product_params)
+    @business = Business.find_by_id(params[:business][:id])
+    respond_to do |format|
+      if @product.valid?
+        @business << @product
+        Tag.find_or_create_by(name: params[:tag][:name])
+        @product.save
+        raise params.inspect
+        format.html { redirect_to business_path(@business), notice: "New product added." }
+      else
+        format.html { render :new }
+        #add error message
+      end
+    end
+  end
+
   def edit
     @product = Product.find_by_id(params[:id])
   end
@@ -20,6 +38,15 @@ class ProductsController < ApplicationController
   end
 
   def delete
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(
+      :name,
+      :tags => [:name]
+    )
   end
 
 end
