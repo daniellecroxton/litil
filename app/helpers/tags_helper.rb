b27ae00f19793406cc8658be3a9f170413a7f8aa
daniellecroxton <danikrist@gmail.com>
2018-05-27 15:41:33 -0600

    updated the way tags render on the product show page

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -1,8 +1,6 @@
 module TagsHelper
   def product_tag_links(product)
-    links = product.tags.each do |tag|
-      link_to tag.name, tag_path(tag)
-    end
-    links.to_sentence
+    links = product.tags.map{ |tag| link_to(tag.name, tag_path(tag)).html_safe }
+    safe_join(links, ', ')
   end
 end
